da4e8e1dcb22f52396db2e82b550d7b95fe0284a
Manish Jha <manishjhazyx@gmail.com>
2024-05-02 19:40:45 +0530

    feat: rainbowkit wallets authentication + initial setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ["en-US", "zh-CN", "en-IN"],
+    defaultLocale: "en-US",
+  },
 };
 
 export default nextConfig;
