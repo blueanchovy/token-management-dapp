35f91ab662afe470822411c3d30dd3ba8c1b380b
Manish Jha <manishjhazyx@gmail.com>
2024-05-16 23:47:20 +0530

    feat: added header styling and material ui

--- components/TokenMarqueeTicker.tsx
+++ components/TokenMarqueeTicker.tsx
@@ -31,20 +31,20 @@ const TokenMarqueeTicker: React.FC = () => {
   }, []);
 
   return (
-    <div className="mt-4 w-full">
+    <div className="mt-4 w-full bg-[#0e0f15] border-t-[0.5px] border-indigo-500">
       <div className="overflow-x-hidden">
         <div className="flex animate-marquee whitespace-nowrap hover:[animation-play-state:paused]">
           {[...tokens].map((token, index) => (
             <div
               key={`${token.tokenAddress}-${index}`}
-              className="flex items-center space-x-4 px-8 py-2 mr-4 bg-gray-800 text-white rounded-lg cursor-pointer"
+              className="flex items-center space-x-4 px-4 py-2 mr-4 text-white rounded-lg cursor-pointer"
             >
               {/* <img
                 src={`https://example.com/token/${token.tokenSymbol}.png`}
                 alt={`${token.tokenSymbol} icon`}
                 className="w-6 h-6"
               /> */}
-              <p>{token.tokenSymbol}</p>
+              <p className="text-blue-400">{token.tokenSymbol}</p>
               <p>Max Price: $ {getFormattedBalance(token.maxPrice)}</p>
             </div>
           ))}
